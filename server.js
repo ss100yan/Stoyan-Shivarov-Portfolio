1e65960e7cc1da24843ad87c0fd80729ef26ca87
ss100yan <32798618+ss100yan@users.noreply.github.com>
2019-04-08 10:50:04 -0400

    new

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + '/public'));
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+var routes = require('./controller/controller.js');
+app.use('/', routes);
+
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+  console.log('Listening on PORT ' + port);
+});
